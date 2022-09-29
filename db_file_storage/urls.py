dfcc20180d051434a922e88d39fecbea5b0d4b18
James Walters <33947683+iamjameswalters@users.noreply.github.com>
2022-09-29 12:33:34 -0400

    Converted url() to path()

--- db_file_storage/urls.py
+++ db_file_storage/urls.py
@@ -1,11 +1,11 @@
 # project
 from . import views
-from django.urls import path
+from .compat import url
 
 
 urlpatterns = [
-    path('download/', views.get_file, {'add_attachment_headers': True},
+    url(r'^download/', views.get_file, {'add_attachment_headers': True},
         name='db_file_storage.download_file'),
-    path('get/', views.get_file, {'add_attachment_headers': False},
+    url(r'^get/', views.get_file, {'add_attachment_headers': False},
         name='db_file_storage.get_file')
 ]
